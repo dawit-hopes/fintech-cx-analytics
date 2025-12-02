307b49c0f3f7cb4ddd14b78d0dcaeedff506fc8d
dawit-hopes <dawit.abrahame@gmail.com>
2025-12-02 22:41:03 +0300

    feat: creatd table classes

---
+++ src/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Banks(Base):
+    __tablename__ = "bank_reviews"
+    bank_id = Column(Integer, primary_key=True)
+    bank_name = Column(String)
+    app_name = Column(String)
+    
+
+class Reviews(Base):
+    __tablename__ = "reviews"
+    review_id = Column(Integer, primary_key=True)
+    rating = Column(Integer)
+    bank_id = Column(Integer, ForeignKey("bank_reviews.bank_id"))
+    review_text = Column(String)
+    review_date = Column(Date)
+    sentiment_label = Column(String)
+    sentiment_score = Column(String)
+    source = Column(String)
